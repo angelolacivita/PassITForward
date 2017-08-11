01633a7cb17ef0bbd6dc1718e1dea818b6e56d7d
MattMenna <mattmenna@gmail.com>
2017-08-11 15:05:33 -0400

    Multiple Controllers

--- src/main/java/com/gc/controller/StringMappings.java
+++ src/main/java/com/gc/controller/StringMappings.java
@@ -24,37 +24,37 @@ public class StringMappings {
     @RequestMapping("/") // returns the login page
     //the String method returns the jsp page that we want to show
     public String login2() {
+
         return "login";
     }
 
     @RequestMapping("/about") // needs copy
     //the String method returns the jsp page that we want to show
     public String about() {
-
         return "about";
     }
 
     @RequestMapping("/contact")
     //the String method returns the jsp page that we want to show
     public String contact() {
-
         return "contact";
     }
 
     @RequestMapping("/submitslackmessage")
     public String submitslackmessage() {
+
         return "slackmessage";
     }
 
     @RequestMapping("/tempPage")
     public String tempPage() {
+
         return "tempPage";
     }
 
     @RequestMapping("/registrationsuccess")
     //the String method returns the jsp page that we want to show
     public String registrationsuccess() {
-
         return "registrationsuccess";
     }
 
